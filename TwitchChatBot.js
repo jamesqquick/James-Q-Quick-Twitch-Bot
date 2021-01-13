7a9e9ebd8ee1ddf3f525cdb36ea73857e8bd7a50
James Quick <james.q.quick@outlook.com>
2021-01-13 11:31:30 -0600

    Installed missing packages

--- TwitchChatBot.js
+++ TwitchChatBot.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
-const {
-    QuickTwitchBot,
-} = require('../Auth0-Bot copy/node_modules/quick-chat-bot');
+const { QuickTwitchBot } = require('quick-chat-bot');
 const path = require('path');
 const bot = new QuickTwitchBot({
     username: process.env.TWITCH_USERNAME,
